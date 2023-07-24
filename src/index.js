690fd48b5ab48b0c7c21ab10c5deeba73afd0f48
KFX <kfx0000@gmail.com>
2023-07-24 20:23:43 +0300

    fix: minor fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  const towel = (result, subs, n) => result.concat((n % 2 ? subs.reverse():subs));
+  const towel = (result, subs, n) => result.concat(n % 2 ? subs.reverse():subs);
   if(matrix === undefined) return [];
   else return matrix.reduce(towel, []);
 }
