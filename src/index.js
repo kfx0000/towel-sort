ca6928018954a5d9f686324c1981ca77bf0400d7
KFX <kfx0000@gmail.com>
2023-07-24 19:41:45 +0300

    fix: task complete

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  const towel = (result, subs, n) => {return result.concat((n % 2 ? subs.reverse():subs));}
+  if(matrix === undefined) return [];
+  else return matrix.reduce(towel, []);
 }
